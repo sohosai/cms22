4b950d8669bff4bdb80f3a7e1e5b190fada4d6bc
Yusei Ito <me@yuseiito.com>
2022-08-27 00:14:11 +0900

    Load firebase settings from environment variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,17 +6,20 @@ import router from './router'
 import { createGtm } from '@gtm-support/vue-gtm'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
 
-if (location.hostname === 'localhost' && process.env.VUE_APP_FIREBASE_EMULATORS !== "disable") {
+if (
+  location.hostname === 'localhost' &&
+  process.env.VUE_APP_FIREBASE_EMULATORS !== 'disable'
+) {
   firebase.auth().useEmulator('http://localhost:9099')
   firebase.firestore().useEmulator('localhost', 8080)
   firebase.functions().useEmulator('localhost', 5001)
